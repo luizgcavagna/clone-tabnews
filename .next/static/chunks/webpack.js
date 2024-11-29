439a95210a0d6bb9356b363223dbcf2aaf4489b1
Luiz Gustavo Cavagna <luizcavagna@gmail.com>
2024-11-29 15:39:11 +0000

    index.js hello World

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -107,7 +107,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c5ef75dc11c2b994"; }
+/******/ 		__webpack_require__.h = function() { return "fa14cec0016dcc8a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
